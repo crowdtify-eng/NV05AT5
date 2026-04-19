8df8d7c50914301bcf14f34cff6e696e216471cd
crowdtify-eng <crowdtify@gmail.com>
2026-04-19 16:01:25 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
       <Testimonials />
       <CTA />
       <Contact />
-   <LocationSection />
+      <LocationSection />
       <Footer />
       <WhatsAppButton />
     </main>
